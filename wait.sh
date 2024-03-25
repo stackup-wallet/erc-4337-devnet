a25cf819999ec984bc1b139e93b6125aff652918
hazim-j <hazim.jumali@gmail.com>
2024-03-25 16:59:06 +1100

    Add eth_supportedEntryPoints to wait script

--- wait.sh
+++ wait.sh
@@ -1,27 +1,48 @@
 #!/bin/bash
 
-NODE_URL="http://localhost:8545"
-JSONRPC_REQUEST='{
+PROXY_URL="http://localhost:8545"
+BUNDLER_REQUEST='{
     "jsonrpc": "2.0",
     "id": 1,
-    "method": "eth_getCode",
-    "params": ["0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789", "latest"]
+    "method": "eth_supportedEntryPoints",
+    "params": []
 }'
 
 while true; do
-    RESPONSE=$(curl -s -X POST -H "Content-Type: application/json" --data "$JSONRPC_REQUEST" "$NODE_URL")
+    BUNDLER_RESP=$(curl -s -X POST -H "Content-Type: application/json" --data "$BUNDLER_REQUEST" "$PROXY_URL")
     if [ $? -ne 0 ]; then
-        echo "Error: Failed to connect to the blockchain node. Retrying in 3 seconds..."
+        echo "Error: Failed to make bundler request. Retrying in 3 seconds..."
         sleep 3
         continue
     fi
 
-    CODE_HEX=$(echo "$RESPONSE" | jq -r '.result')
-    if [ $CODE_HEX != "0x" ]; then
-        echo "ERC-4337 Devnet is ready."
-        break  # Exit the loop once the condition is met.
-    else
-        echo "ERC-4337 Devnet is not ready. Checking again soon..."
-        sleep 3
+    READY=false
+    SUPPORTED_ENTRYPOINTS=()
+    while IFS= read -r line; do
+        SUPPORTED_ENTRYPOINTS+=("$line")
+    done < <(echo "$BUNDLER_RESP" | jq -r '.result[]')
+    for ENTRYPOINT in "${SUPPORTED_ENTRYPOINTS[@]}"; do
+        NODE_REQUEST="{
+            \"jsonrpc\": \"2.0\",
+            \"id\": 1,
+            \"method\": \"eth_getCode\",
+            \"params\": [\"$ENTRYPOINT\", \"latest\"]
+        }"
+        NODE_RESP=$(curl -s -X POST -H "Content-Type: application/json" --data "$NODE_REQUEST" "$PROXY_URL")
+
+
+        CODE_HEX=$(echo "$NODE_RESP" | jq -r '.result')
+        if [ $CODE_HEX != "0x" ]; then
+            echo "ERC-4337 Devnet is ready."
+            READY=true
+        else
+            echo "ERC-4337 Devnet is not ready. Checking again soon..."
+            READY=false
+        fi
+    done
+
+    if [ "$READY" == "true" ]; then
+        exit 0;
     fi
+    sleep 3
 done
