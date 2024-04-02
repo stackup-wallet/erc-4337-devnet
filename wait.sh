64bee3afed651c1f9dbe97b6e28b08f608869d2c
hazim <hazim.jumali@gmail.com>
2024-04-02 15:42:51 +1100

    Add stackup-paymaster to the devnet (#9)

--- wait.sh
+++ wait.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 PROXY_URL="http://localhost:8545"
+PM_URL="http://localhost:43371"
 while true; do
     # Check Bundler HTTP server is ready.
     BUNDLER_REQ='{
@@ -11,11 +12,11 @@ while true; do
     }'
     BUNDLER_RESP=$(curl -s -X POST -H "Content-Type: application/json" --data "$BUNDLER_REQ" "$PROXY_URL")
     if [ $? -ne 0 ]; then
-        echo "Error: Failed to make bundler request. Retrying in 3 seconds..."
+        echo "Error: Failed to make bundler request. Checking again soon..."
         sleep 3
         continue
     elif ! echo $BUNDLER_RESP | jq . > /dev/null 2>&1; then
-        echo "Error: Bundler response is not valid JSON. Retrying in 3 seconds..."
+        echo "Error: Bundler response is not valid JSON. Checking again soon..."
         sleep 3
         continue
     fi
@@ -35,7 +36,8 @@ while true; do
         }"
         NODE_RESP=$(curl -s -X POST -H "Content-Type: application/json" --data "$NODE_REQ" "$PROXY_URL")
         if [ $? -ne 0 ]; then
-            echo "Error: Failed to make node request. Retrying in 3 seconds..."
+            echo "Error: Failed to make node request. Checking again soon..."
+            READY=false
             break
         fi
 
@@ -44,27 +46,30 @@ while true; do
         if [ $CODE_HEX == "0x" ]; then
             echo "ERC-4337 Devnet EntryPoint not yet deployed. Checking again soon..."
             READY=false
+            break
+        fi
+
+        # Check Stackup Paymaster v0.6 is ready.
+        STACKUP_PM_REQ="{
+            \"jsonrpc\": \"2.0\",
+            \"id\": 1,
+            \"method\": \"pm_accounts\",
+            \"params\": [\"$ENTRYPOINT\"]
+        }"
+        STACKUP_PM_RESP=$(curl -s -X POST -H "Content-Type: application/json" --data "$STACKUP_PM_REQ" "$PM_URL")
+        if [ $? -ne 0 ]; then
+            echo "Error: Failed to make paymaster request. Checking again soon..."
+            READY=false
+            break
+        elif ! echo $STACKUP_PM_RESP | jq . > /dev/null 2>&1; then
+            echo "Error: Paymaster response is not valid JSON. Checking again soon..."
+            READY=false
+            break
+        elif [ $(echo $STACKUP_PM_RESP | jq -r '.result[0]') != "0x42051Fa8F6c012102899c902aA214f1e97bD8aDb" ]; then
+            echo "Error: Unexpected paymaster accounts."
+            exit 1;
         fi
     done
-
-    # Check Stackup Paymaster v0.6 contract is deployed.
-    STACKUP_PM_REQ='{
-        "jsonrpc": "2.0",
-        "id": 1,
-        "method": "eth_getCode",
-        "params": ["0x42051Fa8F6c012102899c902aA214f1e97bD8aDb", "latest"]
-    }'
-    STACKUP_PM_RESP=$(curl -s -X POST -H "Content-Type: application/json" --data "$STACKUP_PM_REQ" "$PROXY_URL")
-    if [ $? -ne 0 ]; then
-        echo "Error: Failed to make node request. Retrying in 3 seconds..."
-        sleep 3
-        continue
-    fi
-    STACKUP_PM_CODE_HEX=$(echo "$STACKUP_PM_RESP" | jq -r '.result')
-    if [ $STACKUP_PM_CODE_HEX == "0x" ]; then
-        echo "ERC-4337 Devnet Stackup Paymaster not yet deployed. Checking again soon..."
-        READY=false
-    fi
 
     # Check if devnet is ready.
     if [ "$READY" == "true" ]; then
